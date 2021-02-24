2516d7e4996902b738d3b205dc64d03376994c40
HarrisonOssias <ossiash@mcmaster.ca>
2021-02-24 14:54:20 -0500

    updated directory tree

--- MacHouse/App.js
+++ MacHouse/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
-import Calendar from "./screens/Calendar";
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
+import Calendar from './screens/Calendar';
 
 export default function App() {
-  return <Calendar></Calendar>;
+	return <Calendar></Calendar>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-  },
+	container: {
+		backgroundColor: '#fff',
+	},
 });
